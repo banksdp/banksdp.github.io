d8f4243cebabe82b72673252fca8f7e1f8885b39
odaiclet <odaiclet@berkeley.edu>
2024-11-24 16:48:39 -0600

    fixed bugs, added payment w venmo

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const dropdown = document.querySelector(".dropdown");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+});
+
+dropdown.addEventListener("click", (e) => {
+    if (window.innerWidth < 768) {
+    e.preventDefault();
+    dropdown.classList.toggle("active");
+    }
 });
